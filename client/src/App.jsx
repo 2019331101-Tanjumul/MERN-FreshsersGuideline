0eac912c23b86670309c80e76dfd65d4ca45c49f
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-03-24 06:42:57 +0600

    signIn && signUp not changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter ,Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
-function App() {
+
+export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-red-400'>App</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Sign-in' element={<SignIn />} />
+        <Route path='/Sign-up' element={<SignUp />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/projects' element={<Projects />} />
+
+          </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App

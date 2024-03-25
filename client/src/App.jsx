68615d87446d1f0d46822238167c22e412e4d5df
2019331101-Tanjumul <crackerroot4@gmail.com>
2024-03-25 09:06:13 +0600

    Footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
           <Route path='/projects' element={<Projects />} />
 
           </Routes>
+          <Footer />
     </BrowserRouter>
   )
 }
